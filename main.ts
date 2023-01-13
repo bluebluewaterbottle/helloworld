222fdc5e2d3e5920b46f0fc05b09ae12df2dc268
bluebluewaterbottle <122560360+bluebluewaterbottle@users.noreply.github.com>
2023-01-13 13:29:59 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showString("Hello!")
+    basic.showIcon(IconNames.Happy)
+    basic.showArrow(ArrowNames.East)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
 })
-basic.showIcon(IconNames.Silly)
+basic.showIcon(IconNames.Ghost)
+music.setVolume(82)
+music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
